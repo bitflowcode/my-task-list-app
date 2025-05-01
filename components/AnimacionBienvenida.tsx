382af69b9d8f580d7fc60a07650c100870a43110
decodeapp <soporte@decodeapp.net>
2025-05-01 20:52:51 +0200

    fix Animacion Bienvenida useEffect

--- components/AnimacionBienvenida.tsx
+++ components/AnimacionBienvenida.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 
 export default function AnimacionBienvenida() {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch("/animaciones/tareas-animacion.json")
+      .then((res) => res.json())
+      .then(setAnimationData)
+      .catch(console.error);
+  }, []);
+
+  if (!animationData) return null;
+
   return (
     <div className="flex justify-center mb-4">
-      <Lottie
-        animationData={require("../../public/animaciones/tareas-animacion.json")}
-        loop={true}
-        style={{ height: 180 }}
-      />
+      <Lottie animationData={animationData} loop={true} style={{ height: 180 }} />
     </div>
   );
 }
